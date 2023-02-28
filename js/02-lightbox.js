203adf723c164b20b1ff2b67c40038d14b3dfe19
HelenBlyzniuk <helenblyzniuk2@gmail.com>
2023-02-28 09:33:37 +0300

    gallery elements are created and added to HTML,the library is connected

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,47 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+/* <a class="gallery__item" href="large-image.jpg">
+  <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+</a> */
+
+const galleryListEl = document.querySelector(".gallery");
+
+galleryListEl.addEventListener('click', onGalleryListElClick);
+
+function createGalleryElement(array) {
+    
+   return array.map(({ preview, original, description }) => {
+        const item = document.createElement('a');
+        
+        item.href = original;
+        item.classList.add('gallery__item');
+        const image = document.createElement("img");
+        image.src = preview;
+        image.classList.add('gallery__image');
+        image.alt =description;
+        item.append(image);
+        
+       return item;
+    })
+   
+}
+
+const galleryElements = createGalleryElement(galleryItems);
+galleryListEl.append(...galleryElements);
+
+
+function onGalleryListElClick(e) {
+  
+    e.preventDefault();
+    
+    if (e.target.nodeName !== "IMG") {
+        return
+    };
+    
+
+    console.log(e.target);
+}
+
+
+// console.log(createGalleryElement(galleryItems));
