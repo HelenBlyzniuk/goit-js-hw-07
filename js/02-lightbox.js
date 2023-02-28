2aefb59ac382e35aaea85d2f46728947789ef5b6
HelenBlyzniuk <helenblyzniuk2@gmail.com>
2023-02-28 19:21:00 +0300

    task 2 is completed

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,6 +5,9 @@ console.log(galleryItems);
 
 
 const galleryListEl = document.querySelector(".gallery");
+galleryListEl.setAttribute('uk-lightbox', 'caption-position:bottom');
+
+console.log(galleryListEl)
 
 galleryListEl.addEventListener('click', onGalleryListElClick);
 
@@ -14,11 +17,15 @@ function createGalleryElement(array) {
         const item = document.createElement('a');
         
         item.href = original;
-        item.classList.add('gallery__item');
+       item.classList.add('gallery__item');
+       item.dataset.caption = description;
         const image = document.createElement("img");
         image.src = preview;
         image.classList.add('gallery__image');
-        image.alt =description;
+       image.alt = description;
+       image.title = description;
+       image.delay = 250;
+       image.titlePosition = 'top';
         item.append(image);
         
        return item;
@@ -39,8 +46,17 @@ function onGalleryListElClick(e) {
     };
     
 
-    console.log(e.target);
+    let href = (e.target.closest('a').getAttribute('href'));
+    return href;
+ 
 }
+let gallery = new SimpleLightbox('.gallery a');
+
+gallery.on('show.simplelightbox', function () {
+});
+
+gallery.on('error.simplelightbox', function (e) {
+	console.log(e);
+});
 
 
-// console.log(createGalleryElement(galleryItems));
