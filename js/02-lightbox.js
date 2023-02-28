8dfbb07dfd7371f03702ac88b628b3faded20d50
HelenBlyzniuk <helenblyzniuk2@gmail.com>
2023-02-28 09:35:21 +0300

    unnecessary is removed

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,9 +2,7 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
-/* <a class="gallery__item" href="large-image.jpg">
-  <img class="gallery__image" src="small-image.jpg" alt="Image description" />
-</a> */
+
 
 const galleryListEl = document.querySelector(".gallery");
 
